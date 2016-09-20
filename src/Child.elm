484dbb6672f4025cc98eb779f27ee8f294b092ff
jjelfs <jjelfs@travelrepublic.co.uk>
2016-09-20 21:57:04 +0100

    trying to use flexbox - bit baffling

--- src/Child.elm
+++ src/Child.elm
@@ -5,29 +5,28 @@ import Types exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-btn child fn n =
-    button
-        [ onClick (UpdateChild { child | balance = fn child.balance n }) ]
-        [ text (toString n) ]
-
 root: Model -> Html Msg
 root model =
     case model.selectedChild of
         Nothing -> div [] []
         Just child ->
             div
-                []
+                [ class "container selected-child" ]
                 [ div
                     [ class "score-btns good" ]
-                    ([1..3]
-                        |> List.map (btn child (+)))
+                    [ button
+                        [ onClick (UpdateChild { child | balance = child.balance + 1 }) ]
+                        [ text "Nice" ]
+                    ]
                 , div
                     [ class "current-score" ]
                     [ text (child.name ++ " : " ++ (toString child.balance)) ]
                 , div
                     [ class "score-btns bad" ]
-                    ([1..3]
-                        |> List.map (btn child (-)))
+                    [ button
+                        [ onClick (UpdateChild { child | balance = child.balance - 1 }) ]
+                        [ text "Naughty" ]
+                    ]
                 , div
                     [ class "back-btn" ]
                     [ button
